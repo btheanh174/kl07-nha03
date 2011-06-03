27dd6c75cede6f128d8ace43b5e9b679e8ae78bc
lelongho89 <lelongho89@gmail.com>
2011-06-03 05:07:43 +0000

    Duyệt lấy danh sách hóa đơn theo thành viên.

--- SubEclipse/0712152-0712193/src/model/dao/HoaDonDAO.java
+++ SubEclipse/0712152-0712193/src/model/dao/HoaDonDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import model.pojo.ChiTietHoaDon;
 import model.pojo.GianHang;
 import model.pojo.HoaDon;
+import model.pojo.ThanhVien;
 
 import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
@@ -68,6 +69,36 @@ public class HoaDonDAO extends AbstractDAO{
 		}finally{
 			HibernateUtil.shutdown();
 		}
+		return kq;
+	}
+	
+	public List<HoaDon> layDanhSach(ThanhVien thanhVien){
+		List<HoaDon> kq = null;
+		
+		try{
+			session = HibernateUtil.getSessionFactory().getCurrentSession();
+			tx = session.beginTransaction();
+			
+			kq = session.createQuery("from HoaDon as hd where hd.thanhVien =:tv")
+			.setParameter("tv", thanhVien)
+			.list();
+			for (HoaDon hoaDon : kq) {
+				Hibernate.initialize(hoaDon);
+				Hibernate.initialize(hoaDon.getNguoiNhan());
+				Hibernate.initialize(hoaDon.getThanhVien());
+				Hibernate.initialize(hoaDon.getTinhTrang());
+				Hibernate.initialize(hoaDon.getHinhThuc());
+				Hibernate.initialize(hoaDon.getPhuongThuc());
+				Hibernate.initialize(hoaDon.getDsChiTietHoaDon());
+			}
+			
+			tx.commit();
+		}catch(HibernateException e){
+			handleException(e);
+		}finally{
+			HibernateUtil.shutdown();
+		}
+		
 		return kq;
 	}
 	
