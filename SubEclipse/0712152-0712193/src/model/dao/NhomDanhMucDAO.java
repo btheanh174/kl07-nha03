3125fd34bbc3b90c9d8a155c64b3abf8d91369e2
lelongho89 <lelongho89@gmail.com>
2011-06-11 06:01:56 +0000

    Them NhomDanhMucDAO

---
+++ SubEclipse/0712152-0712193/src/model/dao/NhomDanhMucDAO.java
@@ -0,0 +1,69 @@
+package model.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.pojo.GianHang;
+import model.pojo.NhomDanhMuc;
+
+import org.hibernate.Hibernate;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+
+import util.HibernateUtil;
+
+public class NhomDanhMucDAO extends AbstractDAO{
+
+	public NhomDanhMucDAO() {
+		super();
+	}
+	
+	public NhomDanhMuc lay(int id){
+		return (NhomDanhMuc) super.find(NhomDanhMuc.class, id);
+	}
+	
+	public List<NhomDanhMuc> layDanhSach(){
+		return super.findAll(NhomDanhMuc.class);
+	}
+	
+	public List<NhomDanhMuc> layDanhSach(GianHang gianHang){
+		List<NhomDanhMuc> kq = new ArrayList<NhomDanhMuc>();
+		try{
+			
+			session = HibernateUtil.getSessionFactory().getCurrentSession();
+			tx = session.beginTransaction();
+			
+			String hql = "from NhomDanhMuc as ndm where ndm.gianHang =:nhom";
+			Query query = session.createQuery(hql);
+			query.setParameter("nhom", gianHang);
+	
+			kq = query.list();
+			
+			for (NhomDanhMuc nhomDanhMuc : kq) {
+				Hibernate.initialize(nhomDanhMuc);
+				Hibernate.initialize(nhomDanhMuc.getGianHang());
+			}
+			
+			tx.commit();
+			
+		}catch(HibernateException e){
+			handleException(e);
+		}finally{
+			HibernateUtil.shutdown();
+		}
+		
+		return kq;
+	}
+	
+	public void them(NhomDanhMuc nhomDanhMuc){
+		super.saveOrUpdate(nhomDanhMuc);
+	}
+	
+	public void xoa(NhomDanhMuc nhomDanhMuc){
+		super.delete(nhomDanhMuc);
+	}
+	
+	public void capNhat(NhomDanhMuc nhomDanhMuc){
+		super.saveOrUpdate(nhomDanhMuc);
+	}
+}
